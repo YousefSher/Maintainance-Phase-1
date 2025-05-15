7a776ecf44d46f4d1af26b4ef944271cb82a796e
ZiadMedhat33 <ziadmedhat301@gmail.com>
2025-05-15 19:55:47 +0300

    Revert "doing some sonarqube suggestions and adding admin controller"
    This reverts commit ff69d706edb7cc339a8867451c03eca394b3dce2.

--- SpringBootLMS/Learning_Management_System/src/main/java/com/learning_management_system/repository/QuestionTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learning_management_system.repository;
-
-import com.learning_management_system.entity.QuestionType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionTypeRepository extends JpaRepository<QuestionType, Integer> {
-}
