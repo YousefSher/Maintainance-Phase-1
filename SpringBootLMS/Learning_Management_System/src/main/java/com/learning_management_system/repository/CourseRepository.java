0ff7986782fcf265328719be25cbff3e283ac1d4
ZiadMedhat33 <ziadmedhat301@gmail.com>
2025-05-18 15:36:36 +0300

    A

--- SpringBootLMS/Learning_Management_System/src/main/java/com/learning_management_system/repository/CourseRepository.java
+++ SpringBootLMS/Learning_Management_System/src/main/java/com/learning_management_system/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package com.learning_management_system.repository;
 import com.learning_management_system.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
